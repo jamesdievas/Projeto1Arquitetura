b97fe032a71e4b0d6b4c02a1a45abedfa2747542
Henrique <hss@outlook.com.br>
2020-09-03 09:59:20 -0300

    Inserção da função de ordenação
    Função que ordena os RAs

--- projeto1.s
+++ projeto1.s
@@ -2,13 +2,10 @@
     RAs:    .asciiz "\nInsira o RA do aluno "
     pontos: .asciiz ": "
     menu:   .asciiz "\n0 - Encerrar programa.\n1 - Cadastrar notas.\n2 - Excluir notas.\n3 - Exibir notas.\n4 - média aritmética da turma.\n5 - Aprovados.\n-> "
-    vetorRA:  .word 0,0,0,0,0
+    space: .asciiz " "
+    vetorRA:  .word 0, 0, 0, 0, 0
     sizevetRA: .byte 20
-    matriz: .float 0,0,0,0,0,0,0,0,
-                   0,0,0,0,0,0,0,0,
-                   0,0,0,0,0,0,0,0,
-                   0,0,0,0,0,0,0,0,
-                   0,0,0,0,0,0,0,0
+    matriz: .float 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
     sizemat: .word 160
     consti: .byte 32
     constj: .byte 4
@@ -18,7 +15,11 @@ main:
     
     jal RegRA                       # Coletar RAs
 
-    jal bbSort                      #ordena o vetor de RA's
+    jal Exibir
+
+    jal sort                        # Ordena o vetor de RA's
+
+    jal Exibir
 
     loopmenu:
         li $v0, 4
@@ -98,6 +99,29 @@ RegRA:
         addi $sp, $sp, 4
         jr $ra
 
+Exibir:	
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    move $t0, $zero
+
+    exibir:
+	li $v0, 4
+	la $a0, space				    # Exibe um espaço para separação dos números
+	syscall
+	
+	li $v0, 1					    # Comando para exibir o valor
+	lw $a0, vetorRA($t0)		    # Carrega o valor para exibir
+	syscall
+	
+	addi $t0, $t0, 4
+	bne $t0, 20, exibir		        # Verifica se chegou ao final do array
+	
+	lw $ra, 0($sp)				    # Retira da pilha o endereço que foi chamada a main
+	addi $sp, $sp, 4
+	
+	jr $ra
+
 #--------------------------------------------------------------------
 
 Cadastrar:
@@ -108,7 +132,7 @@ Cadastrar:
     move $s1, $zero
 
     
-    lw $ra, 0($sp)                  # desempilha o registrador de retorno
+    lw $ra, 0($sp)                  # Desempilha o registrador de retorno
     addi $sp, $sp, 4
     jr $ra
 
@@ -119,7 +143,7 @@ Excluir:
     sw $ra, 0($sp)
 
     
-    lw $ra, 0($sp)                  # desempilha o registrador de retorno
+    lw $ra, 0($sp)                  # Desempilha o registrador de retorno
     addi $sp, $sp, 4
     jr $ra
 
@@ -130,7 +154,7 @@ Exibnotas:
     sw $ra, 0($sp)
 
     
-    lw $ra, 0($sp)                  # desempilha o registrador de retorno
+    lw $ra, 0($sp)                  # Desempilha o registrador de retorno
     addi $sp, $sp, 4
     jr $ra
 
@@ -141,7 +165,7 @@ Exibirmed:
     sw $ra, 0($sp)
 
     
-    lw $ra, 0($sp)                  # desempilha o registrador de retorno
+    lw $ra, 0($sp)                  # Desempilha o registrador de retorno
     addi $sp, $sp, 4
     jr $ra
 
@@ -152,49 +176,55 @@ ExibAprov:
     sw $ra, 0($sp)
 
     
-    lw $ra, 0($sp)                  # desempilha o registrador de retorno
+    lw $ra, 0($sp)                  # Desempilha o registrador de retorno
     addi $sp, $sp, 4
     jr $ra
 #-------------------------------------------------------------------------------------
-bbSort:
-    addi $sp, $sp, -4
-    sw $ra, 0($sp)
+swap:
+	addi $t4, $s1, 4			    # Carrega j+1
+	lw $t1, vetorRA($s1)			# Carrega array[j]
+	lw $t2, vetorRA($t4)			# Carrega array[j+1]
 
-    lb $t0, sizevetRA	 		    #caregga o tamanho em t0
-    rot1:
-        beq $t0, $zero, saida
-        add $t1, $zero, $zero 		#j
-    rot2:
-        blt $t0, $t1, sair1
+	sw $t2, vetorRA($s1)			# Array[j] = array[j+1]
+	sw $t1, vetorRA($t4)			# Array[j+1] = array[j]
+	jr $ra						    # Retorna para o loop
+	
+sort:
+	addi $sp, $sp, -4			    # Armazena na pilha o endereço de retorno
+	sw $ra, 0($sp)
+	
+	lb $s0, sizevetRA				# i = size
+	
+loop1:
+	blt $s0, 1, exit1			    # Verifica se i < 1
+	move $s1, $zero				    # j = 0
+	
+loop2:
+	slt $t0, $s1, $s0			    # Verifica se j < i
+	beq $t0, $zero, exit2		
 
-        lw $t3,vetorRA($t1) 		#Carregando a posicao i do vetor em t3
+	addi $t5, $s1, 4
+	
+	lw $t3, vetorRA($s1)			# $t3 recebe array[j]
+	lw $t4, vetorRA($t5)			# $t4 recebe array[j+1]
 
-        addi $t1, $t1, 4
-        lw $t4, vetorRA($t1)
-        addi $t1, $t1, -4 
+	blt $t3, $t4, continua		    # Se array[j+1] > array[j] está ordenado e continua no loop
 
-        blt $t3, $t4, sair2 		#Se o proximo for maior ele nao troca
-        move $a0, $t1
-        jal swap
+	jal swap
 
-        sair2:									 
-            addi $t1, $t1, 4		#Decrementa j(t1)
-            j rot2 
-    sair1: 
-        addi $t0, $t0, -4			#Decrementa o i(t0)
-        j rot1
-    saida: 
-    lw $ra, 0($sp)
-    addi $sp, $sp, 4
-    jr $ra
-#-------------------------------------------------------------------------------------
-swap: 
-    add $t7, $a0, 4                  #(address of v[k])
-    lw $t2, vetorRA($t7)             #$t2 (temp) = v[k]
-    lw $t6, vetorRA($a0)             #$t6 = v[k+1]
-    sw $t6, vetorRA($t7)             #v[k] = t6 (v[k+1])
-    sw $t2, vetorRA($a0)             #v[k+1] = t2 (temp)
-    jr $ra					
+continua:	
+	addi $s1, $s1, 4			    # Incrementa uma posição em j (4 bytes = 1 word)
+	j loop2
+	
+exit2:
+	addi $s0, $s0, -4			    # Decrementa uma posição do i, (4 bytes = 1 word)
+	j loop1
+	
+exit1:
+	lw $ra, 0($sp)				    # Retira da pilha o endereço de retorno
+	addi $sp, $sp, 4
+	
+	jr $ra
 #-------------------------------------------------------------------------------------
 ######################################################################################
 # RASCUNHO
